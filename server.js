28fa6fd13a4e56d801fd459f54a22137fd6847aa
kanitsharma <kanitshrm09@gmail.com>
2017-10-15 17:54:55 +0530

    Sorted db

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const requestData = require('./utils/requestData')
 const listURL = 'https://www.mangaeden.com/api/list/0/'
 const url = 'mongodb://kt:tsuk1yomi@ds121225.mlab.com:21225/mangalist'
 
+const mycomparator = (a, b) => b.h - a.h
 
 MongoClient.connect(url, (err, db) => {
   console.log('connected')
@@ -13,11 +14,12 @@ MongoClient.connect(url, (err, db) => {
   setInterval(async () => {
     const data = await requestData(listURL)
     const json = JSON.parse(data)
+    json.manga.sort(mycomparator)
     list.update({ "db": true }, {$set: {
       data: json.manga
     }})
     console.log("updated")
-  }, 1800000)
+  }, 10000)
 })
 
 http.createServer((req, res) => {
